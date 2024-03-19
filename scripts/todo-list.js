e797f3b63fd9bddcac5bc5f3f6370b0b60699fd6
Amin <incamin3@gmail.com>
2024-03-19 13:32:09 +0100

    ahead

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -22,7 +22,7 @@ function renderTodoList() {
   });
 
   document.querySelector('.js-todo-list')
-    .innerHTML = todoListHTML;
+    .innerHTML = todoListHTML ;
 
   document.querySelectorAll('.js-delete-todo-button')
     .forEach((deleteButton, index) => {
